665f464684e74d0cf7797903932f71d5cb635bd8
xsamikx-creator <xsamikx@gmail.com>
2026-07-29 00:42:16 +0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'mushig-tracker-v1';
-const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(r => r || fetch(event.request))));
